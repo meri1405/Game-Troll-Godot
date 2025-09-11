14d7854d90917ddc9189acd5016a667f26b0864c
RockMeetsHill <nhattran2016.nt@gmail.com>
2025-09-11 16:45:05 +0700

    Revert "Merge branch 'main' into nhatt2"
    This reverts commit 25756bac49f43980ca71a3c3948fca7c6ad4c791, reversing
    changes made to d02f486992bf0ba375781cad7dd1d15b16a09bdd.

--- UI/game_over.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func _on_again_pressed() -> void:
-	var current_scene_path = get_tree().current_scene.scene_file_path
-	get_tree().change_scene_to_file(current_scene_path)
-
-func _on_home_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
