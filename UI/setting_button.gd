44999076352c05d876578916f5553481a452d2de
tan123asd <tanncse184029@fpt.edu.vn>
2025-09-11 09:57:27 +0700

    setingMenu

--- UI/setting_button.gd
+++
@@ -1,3 +0,0 @@
-extends CanvasLayer
-func _on_setting_button_pressed():
-	$SettingsPanel.visible = not $SettingsPanel.visible
--- level_1.gd
+++ level_1.gd
@@ -2,3 +2,7 @@ extends Node2D
 func _ready():
 	var game_over_scene = preload("res://UI/game_over.tscn").instantiate()
 	add_child(game_over_scene)
+
+	var config = ConfigFile.new()
+	if config.load("user://settings.cfg") == OK:
+		AudioServer.set_bus_mute(0, config.get_value("audio", "muted", false))
