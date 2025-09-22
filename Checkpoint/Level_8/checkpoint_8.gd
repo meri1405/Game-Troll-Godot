d0985c7a34ec6117d0ae6a75a55e51b097fd9730
RockMeetsHill <nhattran2016.nt@gmail.com>
2025-09-22 18:36:35 +0700

    - added the rest of checkpoints except for 5

--- Checkpoint/Level_8/checkpoint_8.gd
+++ Checkpoint/Level_8/checkpoint_8.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var next_level: String = "res://All_Level/Map Level 3/Level 3.tscn"
+@export var next_level: String = "res://All_Level/Map Level 9/level9.tscn"
 @onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
