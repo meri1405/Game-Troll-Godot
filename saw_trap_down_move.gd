f96c6ac7ed8f2d49d70d91b0ebd5bf489d0f3fcf
Nguyễn Gia Huy <huyngse184439@fpt.edu.vn>
2025-09-26 17:00:27 +0700

    .

---
+++ saw_trap_down_move.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed: float = 800.0
+@export var direction: int = 1
+
+var triggered: bool = false
+var start_position: Vector2
+
+func _ready():
+	start_position = global_position
+	visible = true
+	set_physics_process(true)
+
+func _physics_process(delta: float) -> void:
+	if triggered:
+		global_position.y += direction * speed * delta
+		if global_position.y > 600:
+			visible = false
+			set_physics_process(false)
+
+func _on_saw_trigger_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("Saw activated!")
+		triggered = true
+
+func reset_trap():
+	global_position = start_position
+	triggered = false
+	_ready()
