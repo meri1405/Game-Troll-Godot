cad09828b9d18003242aaa42202d93c0aa8fb952
leductuanvt1 <leductuan3404@gmail.com>
2025-10-30 15:13:00 +0700

    update

---
+++ All_Level/Map Level 28/level_drop2.gd
@@ -0,0 +1,27 @@
+extends AnimatableBody2D
+
+@export var speed: float = 888.8
+@export var direction: int = 1
+
+var triggered: bool = false
+var start_position: Vector2
+
+func _ready():
+	start_position = global_position
+
+func _physics_process(delta: float) -> void:
+	if triggered:
+		global_position.y += direction * speed * delta
+	if global_position.y > 230: #đến đoạn này thì chữ sẽ dừng lại và vẫn hiện đó 
+		global_position.y = 230
+		triggered = false
+		
+
+func _on_saw_trigger_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("Saw activated!")
+		triggered = true
+
+func reset_trap():
+	global_position = start_position
+	triggered = false
